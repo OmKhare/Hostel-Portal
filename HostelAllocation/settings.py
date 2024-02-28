e8c30b706e46bf93a3c147e48351e1fdd2e69f9b
Om Khare <omkhare@Oms-MacBook-Air.local>
2024-02-28 21:51:58 +0530

    small change

--- HostelAllocation/settings.py
+++ HostelAllocation/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ec$t8t-rxp$vwl=9wutqtqevhtf9(9wxo$k92_^0dn^!*r+1*#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['vercel.app', '.now.sh', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 
 AUTH_USER_MODEL = 'MainSite.User'
 
